adf35c5c5d20e67f2d59fc9d8575e6630bf3ef34
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-03-10 12:57:47 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,10 +7,11 @@ PATH = _ENV["!plugins_mod_folder_path"].."/"
 
 
 -- Load directories
-local dirs = path.get_directories(PATH)
-for _, dir in ipairs(dirs) do
-    local files = path.get_files(dir)
-    for _, file in ipairs(files) do
-        require(file)
-    end
-end
+Initialize(function()
+    local dirs = path.get_directories(PATH)
+    for _, dir in ipairs(dirs) do
+        local files = path.get_files(dir)
+        for _, file in ipairs(files) do
+            require(file)
+        end
+end)
