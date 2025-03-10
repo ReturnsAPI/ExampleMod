f22c957b167891258bba7165cb9118e665ef2fd2
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-03-10 13:02:02 -0400

    Update blueCircle.lua

--- Items/blueCircle.lua
+++ Items/blueCircle.lua
@@ -1,2 +1,13 @@
 -- Blue Circle
 
+local item = Item.new("blueCircle")
+
+item:set_sprite(Sprite.new("blueCircle", PATH.."Items/Sprites/blueCircle.png", 1, 16, 16))
+item:set_tier(ItemTier.COMMON)
+
+ItemLog.new_from_item(item)
+
+Callback.add(item.on_acquired, function(actor, stack)
+    print("Picked up 'blueCircle'")
+    print(actor, stack)
+end)
