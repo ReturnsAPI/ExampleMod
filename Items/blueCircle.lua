f4b9c55b6fd9da849ff09ce651307a108371da0f
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-03-10 15:48:30 -0400

    Update blueCircle.lua

--- Items/blueCircle.lua
+++ Items/blueCircle.lua
@@ -2,7 +2,7 @@
 
 local item = Item.new("blueCircle")
 
-item:set_sprite(Sprite.new("blueCircle", PATH.."Items/Sprites/blueCircle.png", 1, 16, 16))
+item:set_sprite(Sprite.new("blueCircle", "~/Items/Sprites/blueCircle.png", 1, 16, 16))
 item:set_tier(ItemTier.COMMON)
 
 ItemLog.new_from_item(item)
