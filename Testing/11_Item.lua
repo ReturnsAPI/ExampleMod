a99d90a49fbb2f5ee3ac95b8b350eb259c99ef99
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-03-21 16:57:20 -0400

    Update 11_Item.lua

--- Testing/11_Item.lua
+++ Testing/11_Item.lua
@@ -9,41 +9,44 @@ Initialize(function()
     ItemLog.new_from_item(item)
 
     item:show_properties()
+
+    Callback.add(item.on_acquired, function(actor, stack)
+        print("'blueCircle' on_acquired")
+        print(actor, actor.value)
+        print("Stack: "..stack)
+    end)
+    
+    -- RecalculateStats.add(function(actor, api)
+    --     -- Add 10 maxhp per stack
+    --     local stack = actor:item_count(item)
+    --     api.maxhp_add(10 * stack)
+    -- end)
+    
+    Callback.add(Callback.ON_HIT_PROC, function(actor, victim, hit_info)
+        if actor:item_count(item) <= 0 then return end
+    
+        print("'blueCircle' ON_HIT_PROC")
+        print(victim.hp)
+        print("Stack: "..actor:item_count(item))
+    
+        print(hit_info, hit_info.value, hit_info.RAPI)
+        print(hit_info.x)
+        hit_info.x = 123
+        print(hit_info.x)
+    
+        local inf = hit_info.inflictor
+        print(inf)
+        print(inf.value, inf.RAPI)
+    
+        local a = hit_info.attack_info
+        print(a)
+        print(a.value, a.RAPI)
+    
+        print(a.x)
+        a.x = 123
+        print(a.x)
+    end)
 end)
-
--- Callback.add(item.on_acquired, function(actor, stack)
---     print("'blueCircle' on_acquired")
---     print("Stack: "..stack)
--- end)
-
--- RecalculateStats.add(function(actor, api)
---     -- Add 10 maxhp per stack
---     local stack = actor:item_count(item)
---     api.maxhp_add(10 * stack)
--- end)
-
--- Callback.add(Callback.ON_HIT_PROC, function(actor, victim, hit_info)
---     print("'blueCircle' ON_HIT_PROC")
---     print(victim.hp)
---     print("Stack: "..actor:item_count(item))
-
---     print(hit_info, hit_info.value, hit_info.RAPI)
---     print(hit_info.x)
---     hit_info.x = 123
---     print(hit_info.x)
-
---     local inf = hit_info.inflictor
---     print(inf)
---     print(inf.value, inf.RAPI)
-
---     local a = hit_info.attack_info
---     print(a)
---     print(a.value, a.RAPI)
-
---     print(a.x)
---     a.x = 123
---     print(a.x)
--- end)
 
 -- Return status
 return true
