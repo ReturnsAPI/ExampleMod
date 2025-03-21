fadb570fdc4137463ecaf7085e6866c381fa3c4d
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-03-21 17:18:04 -0400

    Update 10_Initialize.lua

--- Testing/10_Initialize.lua
+++ Testing/10_Initialize.lua
@@ -2,20 +2,23 @@
 
 local exit = true
 
-Initialize(function()
-    print("ExampleMod priority -2")
-end, -2)
+if Initialize then
+    Initialize(function()
+        print("ExampleMod priority -2")
+    end, -2)
 
-Initialize(function()
-    print("ExampleMod priority 10")
-end, 10)
+    Initialize(function()
+        print("ExampleMod priority 10")
+    end, 10)
 
-Initialize(function()
-    print("ExampleMod priority -100")
-end, -100)
+    Initialize(function()
+        print("ExampleMod priority -100")
+    end, -100)
 
-Initialize(function()
-    print("ExampleMod priority 0")
-end)
+    Initialize(function()
+        print("ExampleMod priority 0")
+    end)
+else log.warning("10_Initialize: Initialize does not exist"); exit = false
+end
 
 return exit
