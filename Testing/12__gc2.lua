08aef1b7cc79dee0f4eb16dec43d936d1be51393
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-03-21 16:46:21 -0400

    Create 12__gc2.lua

---
+++ Testing/12__gc2.lua
@@ -0,0 +1,9 @@
+-- __gc test 2
+
+local exit = true
+
+collectgarbage()
+if Map.get_refmap_count() ~= 0 then log.warning("12__gc2: __ref_map size is "..tostring(Map.get_refmap_count()).."; expected 0"); exit = false end
+
+-- Return status
+return exit
