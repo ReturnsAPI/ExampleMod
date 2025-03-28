299c37a5b22235fda944be8ff9fb7f1a14f383ed
Klehrik <78520710+Klehrik@users.noreply.github.com>
2025-03-28 00:14:53 -0400

    Update file.lua

--- Language/english/file.lua
+++ Language/english/file.lua
@@ -2,7 +2,22 @@ return {
     item = {
         blueCircle = {
             name        = "Blue Circle",
-            description = "Does something."
+            pickup      = "Does something.",
+            description = "Does something.",
+            destination = "Some place",
+            date        = "01/01/2056",
+            story       = "Lore entry",
+            -- priority    = "Standard"
+        },
+
+        greenSquare = {
+            name        = "Green Square",
+            pickup      = "Does something.",
+            description = "Does something.",
+            destination = "Some place",
+            date        = "01/01/2056",
+            story       = "Lore entry",
+            -- priority    = "Standard"
         }
     }
 }
